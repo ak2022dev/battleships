1a54659796133e9f2425d8a3c6d3d1d36c5b4c1a
Amir Khan <ak2022dev@gmail.com>
2023-06-22 17:15:41 +0100

    TDD pass first test for creating new game initialised with unplaced ships, which can then be read back from the game.

--- lib/game.rb
+++ lib/game.rb
@@ -1,8 +1,10 @@
 class Game
-  def unplaced_ships
-    []
-  end
+  attr_reader :unplaced_ships
 
+  def initialize(ships_array)
+    @unplaced_ships = ships_array
+  end
+  
   def rows
   end
 
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,4 +1,11 @@
 require_relative "../lib/game"
 
 RSpec.describe Game do
+  context "it manages unplaced ships" do
+    it "stores and returns unplaced_ships" do
+      game = Game.new([2])
+      result = game.unplaced_ships
+      expect(result).to eq([2])
+    end
+  end
 end
