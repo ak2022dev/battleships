104690b890a234b2531a293818653030dbce4a02
Amir Khan <ak2022dev@gmail.com>
2023-06-22 17:17:31 +0100

    TDD add and pass further tests for storing and reading unplaced ships with no changes to code

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -7,5 +7,15 @@ RSpec.describe Game do
       result = game.unplaced_ships
       expect(result).to eq([2])
     end
+    it "stores and returns unplaced_ships" do
+      game = Game.new([1,3])
+      result = game.unplaced_ships
+      expect(result).to eq([1,3])
+    end
+    it "stores and returns unplaced_ships" do
+      game = Game.new([4,2,1])
+      result = game.unplaced_ships
+      expect(result).to eq([4,2,1])
+    end
   end
 end
