84f6d86975d9142209889b3495603058b4226a23
Amir Khan <ak2022dev@gmail.com>
2023-06-23 11:13:39 +0100

    TDD refactor tests in game_spec to accommodate change in constructor for Game

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -3,18 +3,18 @@ require_relative "../lib/board"
 
 RSpec.describe Game do
   context "it manages unplaced ships" do
-    xit "stores and returns unplaced_ships" do
-      game = Game.new([2])
+    it "stores and returns unplaced_ships" do
+      game = Game.new([2], 10)
       result = game.unplaced_ships
       expect(result).to eq([2])
     end
-    xit "stores and returns unplaced_ships" do
-      game = Game.new([1,3])
+    it "stores and returns unplaced_ships" do
+      game = Game.new([1,3], 10)
       result = game.unplaced_ships
       expect(result).to eq([1,3])
     end
-    xit "stores and returns unplaced_ships" do
-      game = Game.new([4,2,1])
+    it "stores and returns unplaced_ships" do
+      game = Game.new([4,2,1], 10)
       result = game.unplaced_ships
       expect(result).to eq([4,2,1])
     end
