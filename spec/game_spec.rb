b97c4c158ee858156b791c035682cc97acd88283
Amir Khan <ak2022dev@gmail.com>
2023-07-05 13:38:24 +0100

    TDD pass game_spec unit test to place a ship length 1 on board, uses mocking, to add unit test to board class

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -34,6 +34,7 @@ RSpec.describe Game do
       expect(result).to eq([4,2,1])
     end
   end
+
   context "it manages a board" do
     it "creates a 10x10 board for the game" do
       game = Game.new([2,5], @board)
@@ -50,15 +51,38 @@ RSpec.describe Game do
                             [0,0,0,0,0,0,0,0,0,0]])
     end
   end
+
   context "it places ships" do
     it "places a ship" do
       # Call place_ship method for game
       # which calls place_ship method for board
       # latter will need to be mocked in game unit test
       # and tested in board unit test
-      expect(@board).to receive(:place_ship).with(length: 2, orientation: :horizontal, row: 3, col: 2)
-      game = Game.new([2,5], @board)
-      game.place_ship(length: 2, orientation: :horizontal, row: 3, col: 2)      
+      expect(@board).to receive(:place_ship).with(length: 1, orientation: :horizontal, row: 3, col: 2)
+      expect(@board).to receive(:contents).and_return([[0,0,0,0,0,0,0,0,0,0],
+                                                       [0,0,0,0,0,0,0,0,0,0],
+                                                       [0,1,0,0,0,0,0,0,0,0],
+                                                       [0,0,0,0,0,0,0,0,0,0],
+                                                       [0,0,0,0,0,0,0,0,0,0],
+                                                       [0,0,0,0,0,0,0,0,0,0],
+                                                       [0,0,0,0,0,0,0,0,0,0],
+                                                       [0,0,0,0,0,0,0,0,0,0],
+                                                       [0,0,0,0,0,0,0,0,0,0],
+                                                       [0,0,0,0,0,0,0,0,0,0]])
+      game = Game.new([1,2,5], @board)
+      game.place_ship(length: 1, orientation: :horizontal, row: 3, col: 2)
+      result = game.board.contents
+      expect(result).to eq([[0,0,0,0,0,0,0,0,0,0],
+                            [0,0,0,0,0,0,0,0,0,0],
+                            [0,1,0,0,0,0,0,0,0,0],
+                            [0,0,0,0,0,0,0,0,0,0],
+                            [0,0,0,0,0,0,0,0,0,0],
+                            [0,0,0,0,0,0,0,0,0,0],
+                            [0,0,0,0,0,0,0,0,0,0],
+                            [0,0,0,0,0,0,0,0,0,0],
+                            [0,0,0,0,0,0,0,0,0,0],
+                            [0,0,0,0,0,0,0,0,0,0]])
     end
   end
+
 end
